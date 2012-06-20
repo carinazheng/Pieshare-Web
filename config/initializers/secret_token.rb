18cda1f1c88aae6aebae8e84864f1bbb9670482f
Carina Zheng <carina.zheng@west.cmu.edu>
2012-06-19 21:09:55 -0700

    US-1 Sign up & Login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ishare::Application.config.secret_token = 'fb268cb0a1448f340022191f989816a9809de2e012732e553e1acc3f5828e05eb5ec37931fc210fa5990b8b4a96fe686d29bfca95813f3e47e79d4ad00577ce5'
